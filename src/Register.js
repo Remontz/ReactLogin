f9ae8fd3b06b6673e9173fe57774c3e86530bb9f
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-09-18 20:24:30 -0500

    fixing package-log.json

--- src/Register.js
+++ src/Register.js
@@ -1,5 +1,5 @@
 import { useRef, useState, useEffect } from 'react'
-import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faTimes, faInfoCircle, faInfo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
@@ -114,6 +114,31 @@ const Register = () => {
                     8 to 24 characters.<br />
                     Must include uppercase and lowercase letters, a number and a special character.<br />
                     Allowed special characters: <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span>
+                </p>
+            </label>
+
+            <label htmlFor='confirm_pwd'>
+                Confirm Password:
+                <span className={validMatch && matchPwd ? 'valid' : 'hide'}>
+                    <FontAwesomeIcon icon={faCheck} />
+                </span>
+                <span className={validMatch || !matchPwd ? 'hide' : 'invalid'}>
+                    <FontAwesomeIcon icon={faTimes} />
+                </span>
+
+                <input
+                    type='password'
+                    id='confirm_pwd'
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    required
+                    aria-invalid={validMatch ? 'false' : 'true'}
+                    aria-describedby='confirmnote'
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'} >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password input field.
                 </p>
             </label>
         </form>
