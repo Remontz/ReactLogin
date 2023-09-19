9a3dcd380fd81e887d8f01389ab842425ac0b38b
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-09-18 22:24:56 -0500

    form fields added - username, pwd, submit, needaccount?

--- client/src/Login.js
+++ client/src/Login.js
@@ -23,6 +23,39 @@ const Login = () => {
     <section>
         <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'> {errMsg} </p>
         <h1>Sign In</h1>
+        <form onSubmit={handleLogin}>
+            <label htmlFor='username'>
+                Username:
+                <input
+                    type='text'
+                    id='username'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
+            </label>
+
+            <label htmlFor='password'>
+                Password:
+                <input
+                    type='password'
+                    id='password'
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+            </label>
+            <button>Sign In</button>
+        </form>
+        <p>
+            Need an Account? <br />
+            <span>
+                {/* put router link here */}
+                <a href='#'>Sign Up</a>
+            </span>
+        </p>
     </section>
   )
 }
