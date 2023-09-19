63160b902c1197520a083ca01a7b0b9e1df969f7
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-09-18 19:01:57 -0500

    refs added for user focus and error focus - form field states added

--- src/Register.js
+++ src/Register.js
@@ -4,3 +4,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+
+
+const Register = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+
+
+    //Field States
+    const [user, setUser] = useState('')
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+
+    const [pwd, setPwd] = useState('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Register
