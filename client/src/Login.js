ab5b83a3f5e427a207037117afccfe94f3502469
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-09-18 22:57:52 -0500

    reformatted for readability

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,100 +1,119 @@
-import {useRef, useState, useEffect, useContext} from 'react'
-import AuthContext from './context/AuthProvider'
-import axios from './api/axios'
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
 
-const LOGIN_URL = '/auth'
+const LOGIN_URL = "/auth";
 
 const Login = () => {
-    const { setAuth } = useContext(AuthContext)
-    const userRef = useRef()
-    const errRef = useRef()
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
 
-
-    const [user, setUser] = useState('')
-    const [pwd, setPwd] = useState('')
-    const [errMsg, setErrMsg] = useState('')
-    const [success, setSuccess] = useState(false)
-
-    useEffect(() => {
-        userRef.current.focus()
-    }, [])
+    const [user, setUser] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
-        setErrMsg('')
-    }, [user, pwd])
+    userRef.current.focus();
+    }, []);
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        
-        try{
-            const response = await axios.post(LOGIN_URL,
-                JSON.stringify({user, pwd}),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-                }
-            )
-            console.log(JSON.stringify(response?.data))    
-            console.log(JSON.stringify(response))
-            const accessToken = response?.data?.accessToken
-            const roles = response?.data?.roles
-            setAuth({user, pwd, roles, accessToken})    
-            setUser('')
-            setPwd('')
-            setSuccess(true)
-        } catch (err) {
-            if(!err?.response) {
-                setErrMsg('No Server Response')
-            } else if(err.response?.status === 400) {
-                setErrMsg('Missing Username or Password')
-            } else if(err.response?.status === 401) {
-                setErrMsg('Unauthorized')
-            } else {
-                setErrMsg('Login Failed')
-            }
-            errRef.current.focus()
+    useEffect(() => {
+    setErrMsg("");
+    }, [user, pwd]);
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
         }
+        );
+        console.log(JSON.stringify(response?.data));
+        console.log(JSON.stringify(response));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({ user, pwd, roles, accessToken });
+        setUser("");
+        setPwd("");
+        setSuccess(true);
+    } catch (err) {
+        if (!err?.response) {
+        setErrMsg("No Server Response");
+        } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+        } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+        } else {
+        setErrMsg("Login Failed");
+        }
+        errRef.current.focus();
     }
+    };
 
-  return (
-    <section>
-        <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'> {errMsg} </p>
-        <h1>Sign In</h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='username'>
+    return (
+    <>
+        {success ? (
+        <section>
+            <h1>You are logged in!</h1>
+            <br />
+            <p>
+            <a href="#">Go to Home</a>
+            </p>
+        </section>
+        ) : (
+        <section>
+            <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+            >
+            {" "}
+            {errMsg}{" "}
+            </p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="username">
                 Username:
                 <input
-                    type='text'
-                    id='username'
-                    ref={userRef}
-                    autoComplete='off'
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                    required
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
                 />
             </label>
 
-            <label htmlFor='password'>
+            <label htmlFor="password">
                 Password:
                 <input
-                    type='password'
-                    id='password'
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    required
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
                 />
             </label>
             <button>Sign In</button>
-        </form>
-        <p>
+            </form>
+            <p>
             Need an Account? <br />
             <span>
-                {/* put router link here */}
-                <a href='#'>Sign Up</a>
+              {/* put router link here */}
+                <a href="#">Sign Up</a>
             </span>
-        </p>
-    </section>
-  )
-}
+            </p>
+        </section>
+        )}
+    </>
+    );
+};
 
-export default Login
+export default Login;
